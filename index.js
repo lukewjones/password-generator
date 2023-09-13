ea38d037b0738623aaf5f92d4ab870ce986198b9
Luke Jones <lukewjones2004@gmail.com>
2023-09-13 15:56:46 -0700

    add major changes such as new features

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
-let pass1 = document.getElementById("pass-one")
-let pass2 = document.getElementById("pass-two")
+let pass1El = document.getElementById("pass-one");
+let pass2El = document.getElementById("pass-two");
+let passLengthEl = document.getElementById("pass-length");
+let symbolsEl = document.getElementById("symbols");
 
 function generatePassword() {
-    let randomPass1 = "";
-    for (let i = 0; i < 14; i++) {
-        randomPass1 += characters[Math.floor(Math.random() * characters.length)];
-        pass1.textContent = randomPass1
+    let passLength = Number(passLengthEl.value);
+    let usingSymbols = symbolsEl.checked;
+    let passChars = [
+        ...characters,
+        ...(usingSymbols ? symbols : [])
+    ];
+    for (let passEl of [pass1El, pass2El]) {
+        let randomPass = "";
+        for (let i = 0; i < passLength; i++) {
+            randomPass += passChars[Math.floor(Math.random() * passChars.length)];
+            passEl.textContent = randomPass;
+            passEl.onclick = function() {
+                navigator.clipboard.writeText(randomPass);
+                passEl.classList.add("pass-copied");
+                setTimeout(function() {
+                    passEl.classList.remove("pass-copied");
+                }, 100);
+            }
+        }
     }
-    let randomPass2 = "";
-    for (let i = 0; i < 14; i++) {
-        randomPass2 += characters[Math.floor(Math.random() * characters.length)];
-        pass2.textContent = randomPass2
-    }
-    return randomPass1
-    return randomPass2
+}
+
+function copyInnerText() {
+
 }
 
 
